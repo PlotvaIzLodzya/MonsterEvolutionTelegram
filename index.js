85faa6f9c14cad18fd76440d158e112bb7e3ad51
Roman Zarnitskiy <gordoy92@gmail.com>
2025-02-10 13:14:12 +0300

    stripping max

---
+++ index.js
@@ -0,0 +1,128 @@
+window.addEventListener("load", function () 
+{
+    if ("serviceWorker" in navigator)
+    {
+      navigator.serviceWorker.register("ServiceWorker.js");
+    }
+  });
+
+  var unityInstanceRef;
+  var unsubscribe;
+  var container = document.querySelector("#unity-container");
+  var canvas = document.querySelector("#unity-canvas");
+  var loadingBar = document.querySelector("#unity-loading-bar");
+  var progressBarFull = document.querySelector("#unity-progress-bar-full");
+  var warningBanner = document.querySelector("#unity-warning");
+
+  // Shows a temporary message banner/ribbon for a few seconds, or
+  // a permanent error message on top of the canvas if type=='error'.
+  // If type=='warning', a yellow highlight color is used.
+  // Modify or remove this function to customize the visually presented
+  // way that non-critical warnings and error messages are presented to the
+  // user.
+  function unityShowBanner(msg, type) 
+  {
+    function updateBannerVisibility()
+    {
+      warningBanner.style.display = warningBanner.children.length ? 'block' : 'none';
+    }
+
+    var div = document.createElement('div');
+    div.innerHTML = msg;
+    warningBanner.appendChild(div);
+
+    if (type == 'error')
+    {
+      div.style = 'background: red; padding: 10px;';
+    }
+    else
+    {
+      if (type == 'warning')
+      {
+        div.style = 'background: yellow; padding: 10px;';
+      }
+
+      setTimeout(function()
+      {
+        warningBanner.removeChild(div);
+        updateBannerVisibility();
+      }, 5000);
+    }
+
+    updateBannerVisibility();
+  }
+
+  var buildUrl = "Build";
+  var loaderUrl = buildUrl + "/MonsterEvolutionTelegram.loader.js";
+  var config = {
+    dataUrl: buildUrl + "/00ee408de191d1a26df9d81ad7922504.data.unityweb",
+    frameworkUrl: buildUrl + "/a5e38da340ffec48fa172dda6cc7b6bd.js.unityweb",
+    codeUrl: buildUrl + "/640c20463340db66daa1d434e511db2f.wasm.unityweb",
+    streamingAssetsUrl: "StreamingAssets",
+    companyName: "DefaultCompany",
+    productName: "MonsterEvolution",
+    productVersion: "0.1",
+    showBanner: unityShowBanner,
+  };
+
+  // By default Unity keeps WebGL canvas render target size matched with
+  // the DOM size of the canvas element (scaled by window.devicePixelRatio)
+  // Set this to false if you want to decouple this synchronization from
+  // happening inside the engine, and you would instead like to size up
+  // the canvas DOM size and WebGL render target sizes yourself.
+  // config.matchWebGLToCanvasSize = false;
+
+  if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent))
+  {
+    // Mobile device style: fill the whole browser client area with the game canvas:
+    var meta = document.createElement('meta');
+    meta.name = 'viewport';
+    meta.content = 'width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, shrink-to-fit=yes';
+    document.getElementsByTagName('head')[0].appendChild(meta);
+  }
+
+  loadingBar.style.display = "block";
+
+  var script = document.createElement("script");
+  script.src = loaderUrl;
+
+  script.onload = () => 
+  {
+    createUnityInstance(canvas, config, (progress) => 
+    {
+      progressBarFull.style.width = 100 * progress + "%";
+    }
+    ).then((unityInstance) => 
+    {
+      unityInstanceRef = unityInstance;
+      loadingBar.style.display = "none";
+    }
+    ).catch((message) => 
+    {
+      alert(message);
+    });
+  };
+
+  document.body.appendChild(script);
+
+  window.addEventListener('load', function ()
+  {
+    Telegram.WebApp.ready();
+    Telegram.WebApp.expand();
+
+    console.log("Telegram Web App has been expanded to full screen");
+
+    var version = Telegram.WebApp.version;
+    var versionFloat = parseFloat(version);
+
+    if (versionFloat >= 7.7)
+    {
+        Telegram.WebApp.disableVerticalSwipes();
+        
+        console.log('Activating vertical swipe disable');
+    }
+
+    console.log(`Telegram Web App opened with version: ${version}`);
+    console.log(`Telegram Web App checked` +
+        `latest version status with result: ${Telegram.WebApp.isVersionAtLeast(version)}`);
+  });
